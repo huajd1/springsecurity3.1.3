e57cd47d989f4c5aeea479fd80d27fe636f73a41
hrrer <130406103+huajd1@users.noreply.github.com>
2023-08-05 23:52:19 +0300

    Delete src directory

--- src/main/java/com/example/spring/security/springsecurity/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.spring.security.springsecurity.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.spring.security.springsecurity.model.Role;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
